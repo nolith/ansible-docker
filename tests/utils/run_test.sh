4a2bef478057af14c98f60003e13e05703b915a4
Alessio Caiazza <alessio@chorally.com>
2016-06-14 11:44:44 +0200

    ansible 2.1 requires docker-py >= 1.7.0

--- tests/utils/run_test.sh
+++ tests/utils/run_test.sh
@@ -7,9 +7,15 @@ docker pull ${IMAGE_FULL}
 docker run --privileged -d --name ansible -v `pwd`:/data ${IMAGE_FULL}
 docker exec -it ansible /bin/sh -c "ansible-playbook /data/tests/test.yml -i /data/tests/inventory --syntax-check"
 docker exec -it ansible /bin/sh -c "ansible-playbook -v /data/tests/test.yml -i /data/tests/inventory -c local"
+echo "Stopping container"
 docker stop ansible
+echo "Committing container"
 docker commit ansible ${PUBLISH_IMAGE}:${VERSION}
+echo "Starting container"
 docker start ansible
+echo "Testing container"
 docker exec -it ansible /bin/sh -c "docker run alpine echo 'docker works'"
+echo "Quay.io login"
 docker login -e="." -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io
+echo "Publishing container"
 docker push ${PUBLISH_IMAGE}:${VERSION}
