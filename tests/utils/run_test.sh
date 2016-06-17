3469add87bb8804d1aae8e823be8ac7bafb4e99e
Alessio Caiazza <alessio@chorally.com>
2016-06-17 12:31:30 +0200

    now docker will start automatically inside the container

--- tests/utils/run_test.sh
+++ tests/utils/run_test.sh
@@ -5,8 +5,8 @@ docker stop ansible >/dev/null 2>&1 || true
 docker rm ansible >/dev/null 2>&1 || true
 docker pull ${IMAGE_FULL}
 docker run --privileged -d --name ansible -v `pwd`:/data ${IMAGE_FULL}
-docker exec -it ansible /bin/sh -c "ansible-playbook /data/tests/test.yml -i /data/tests/inventory --syntax-check"
-docker exec -it ansible /bin/sh -c "ansible-playbook -v /data/tests/test.yml -i /data/tests/inventory -c local"
+docker exec -it ansible /bin/sh -c "ansible-playbook /data/tests/build_container.yml -i /data/tests/inventory --syntax-check"
+docker exec -it ansible /bin/sh -c "ansible-playbook -v /data/tests/build_container.yml -i /data/tests/inventory -c local"
 echo "Stopping container"
 docker stop ansible
 echo "Committing container"
@@ -14,7 +14,7 @@ docker commit ansible ${PUBLISH_IMAGE}:${VERSION}
 echo "Starting container"
 docker start ansible
 echo "Testing container"
-docker exec -it ansible /bin/sh -c "start docker; docker run alpine echo 'docker works'"
+docker exec -it ansible /bin/sh -c "docker run alpine echo 'docker works'"
 echo "Quay.io login"
 docker login -e="." -u="${QUAY_USER}" -p="${QUAY_TOKEN}" quay.io
 echo "Publishing container"
